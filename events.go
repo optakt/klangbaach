ce2c281982a7c14629be540a94ba727c010dee13
Max Wolter <awfm9@protonmail.com>
2022-10-08 01:02:34 -0500

    Add decoding of volumes & liquidity

--- events.go
+++ events.go
@@ -1,8 +1,29 @@
 package main
 
+import (
+	"math/big"
+
+	"github.com/ethereum/go-ethereum/crypto"
+)
+
 const (
-	EventBurn = "Burn(address,uint256,uint256,address)"
-	EventMint = "Mint(address,uint256,uint256)"
 	EventSwap = "Swap(address,uint256,uint256,uint256,uint256,address)"
 	EventSync = "Sync(uint112,uint112)"
 )
+
+var (
+	SigSwap = crypto.Keccak256Hash([]byte(EventSwap))
+	SigSync = crypto.Keccak256Hash([]byte(EventSync))
+)
+
+type Swap struct {
+	Amount0In  *big.Int
+	Amount1In  *big.Int
+	Amount0Out *big.Int
+	Amount1Out *big.Int
+}
+
+type Sync struct {
+	Reserve0 *big.Int
+	Reserve1 *big.Int
+}
--- main.go
+++ main.go
@@ -4,12 +4,14 @@ import (
 	"context"
 	"math/big"
 	"os"
+	"strings"
 	"time"
 
 	"github.com/rs/zerolog"
 	"github.com/spf13/pflag"
 
 	"github.com/ethereum/go-ethereum"
+	"github.com/ethereum/go-ethereum/accounts/abi"
 	"github.com/ethereum/go-ethereum/common"
 	"github.com/ethereum/go-ethereum/ethclient"
 )
@@ -22,6 +24,7 @@ func main() {
 		pairAddress string
 		startHeight uint64
 		batchSize   uint
+		firstStable bool
 	)
 
 	pflag.StringVarP(&apiURL, "api-url", "a", "https://eth-mainnet.g.alchemy.com/v2/UxuHkw-MO02DZ9qYM3usei-qmtlgx8SS", "Ethereum node JSON RPC API URL")
@@ -29,6 +32,7 @@ func main() {
 	pflag.StringVarP(&pairAddress, "pair-address", "p", "0xB4e16d0168e52d35CaCD2c6185b44281Ec28C9Dc", "Ethereum address for Uniswap v2 pair")
 	pflag.Uint64VarP(&startHeight, "start-height", "s", 10019997, "start height for parsing Uniswap v2 pair events")
 	pflag.UintVarP(&batchSize, "batch-size", "b", 100, "number of blocks to cover per request for log entries")
+	pflag.BoolVarP(&firstStable, "first-stable", "f", false, "whether the first symbol in the pair is the stable")
 
 	pflag.Parse()
 
@@ -41,6 +45,11 @@ func main() {
 	log.Level(level)
 
 	log.Info().Msg("starting klangbaach data miner")
+
+	pair, err := abi.JSON(strings.NewReader(ABIPair))
+	if err != nil {
+		log.Fatal().Err(err).Msg("invalid Uniswap Pair ABI")
+	}
 
 	client, err := ethclient.Dial(apiURL)
 	if err != nil {
@@ -64,18 +73,85 @@ func main() {
 			FromBlock: big.NewInt(0).SetUint64(from),
 			ToBlock:   big.NewInt(0).SetUint64(to),
 			Addresses: []common.Address{common.HexToAddress(pairAddress)},
-			Topics:    [][]common.Hash{{SigBurn, SigMint, SigSwap, SigSync}},
+			Topics:    [][]common.Hash{{SigSwap, SigSync}},
 		}
-		logs, err := client.FilterLogs(context.Background(), query)
+
+		entries, err := client.FilterLogs(context.Background(), query)
 		if err != nil {
 			log.Fatal().Err(err).Msg("could not retrieve filtered log entries")
 		}
 
-		log.Info().
-			Uint64("from", from).
-			Uint64("to", to).
-			Int("logs", len(logs)).
-			Msg("filtered log entries retrieved")
+		var swap Swap
+		var sync Sync
+		volumes0 := make(map[uint64]*big.Int)
+		volumes1 := make(map[uint64]*big.Int)
+		liquidities := make(map[uint64]*big.Int)
+		timestamps := make(map[uint64]time.Time)
+		for _, entry := range entries {
+
+			timestamps[entry.BlockNumber] = time.Time{}
+
+			switch entry.Topics[0] {
+
+			case SigSwap:
+
+				err := pair.UnpackIntoInterface(&swap, "Swap", entry.Data)
+				if err != nil {
+					log.Fatal().Err(err).Msg("could not unpack swap event")
+				}
+
+				volume0, ok := volumes0[entry.BlockNumber]
+				if !ok {
+					volume0 = big.NewInt(0)
+				}
+				volume0.Add(volume0, swap.Amount0In)
+
+				volume1, ok := volumes1[entry.BlockNumber]
+				if !ok {
+					volume1 = big.NewInt(0)
+				}
+				volume1.Add(volume1, swap.Amount1In)
+
+				log.Debug().
+					Str("amount0in", swap.Amount0In.String()).
+					Str("amount1in", swap.Amount1In.String()).
+					Str("amount0out", swap.Amount0Out.String()).
+					Str("amount1out", swap.Amount1Out.String()).
+					Str("volume0", volume0.String()).
+					Str("volume1", volume1.String()).
+					Msg("swap decoded")
+
+			case SigSync:
+
+				err := pair.UnpackIntoInterface(&sync, "Sync", entry.Data)
+				if err != nil {
+					log.Fatal().Err(err).Msg("could not unpack sync event")
+				}
+
+				liquidity, ok := liquidities[entry.BlockNumber]
+				if !ok {
+					liquidity = big.NewInt(0)
+				}
+				liquidity.Mul(sync.Reserve0, sync.Reserve1)
+				liquidity.Sqrt(liquidity)
+
+				log.Debug().
+					Str("reserve0", sync.Reserve0.String()).
+					Str("reserve1", sync.Reserve1.String()).
+					Str("liquidity", liquidity.String()).
+					Msg("sync decoded")
+			}
+		}
+
+		for height := range timestamps {
+
+			header, err := client.HeaderByNumber(context.Background(), big.NewInt(0).SetUint64(height))
+			if err != nil {
+				log.Fatal().Uint64("height", height).Err(err).Msg("could not get header for height")
+			}
+
+			timestamps[height] = time.Unix(int64(header.Time), 0)
+		}
 	}
 
 	log.Info().Msg("stopping klangbaach data miner")
